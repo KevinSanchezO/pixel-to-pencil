e7a8580c1556001d5ab8b6936468f7f686dd920f
Kevin Sánchez Obando <kesanchezo@gmail.com>
2023-09-22 12:23:29 -0600

    Modifications in GUI
    Modified "Cantidad de poblaciones" to "Tamaño de poblaciones"

--- menu.py
+++ menu.py
@@ -8,6 +8,9 @@ class Menu(tk.Frame):
         super().__init__(parent, bg="#252525")
         self.controller = controller
 
+        #image to use for the algorithm
+        self.imported_image = None
+
         label1 = tk.Label(self, text="").grid(row=0, column=0,padx=1000,pady=1000)
 
         font_frame = my_font = ctk.CTkFont(size=18)
@@ -29,7 +32,7 @@ class Menu(tk.Frame):
         self.entry_generations.place(x=540, y=110)
 
         #Poblations input
-        label_poblations = ctk.CTkLabel(self, text="Cantidad de poblaciones", fg_color="transparent", font=font_frame)
+        label_poblations = ctk.CTkLabel(self, text="Tamaño de poblaciones", fg_color="transparent", font=font_frame)
         label_poblations.place(x=520, y= 120+50)
 
         self.entry_poblations = ctk.CTkEntry(self, width=150, height=30, font=font_frame, border_width=2)
@@ -39,8 +42,8 @@ class Menu(tk.Frame):
     def load_image(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
         if file_path:
-            image = Image.open(file_path)
-            image = image.resize((400, 400), Image.ANTIALIAS)
+            self.imported_image = Image.open(file_path)
+            image = self.imported_image.resize((400, 400), Image.ANTIALIAS)
             photo = ImageTk.PhotoImage(image)
 
             # Update the label to display the loaded image
